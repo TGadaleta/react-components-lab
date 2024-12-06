324c38c09167d08d3f3041b5b860a1dffee727f9
Tony Gadaleta <anthonygadaleta@gmail.com>
2024-12-06 11:02:34 -0500

    actually finised lab

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -3,7 +3,7 @@ import "./WeatherForecast.css";
 const WeatherForecast = ({weatherForecast}) => {
     return (
         <div className="weather">
-            <h2>Day of the Week</h2>
+            <h2>{weatherForecast.day}</h2>
             <img src={weatherForecast.img} alt={weatherForecast.imgAlt} />
             <p><span>conditions: </span>{weatherForecast.conditions}</p>
             <p><span>time: </span>{weatherForecast.time}</p>
